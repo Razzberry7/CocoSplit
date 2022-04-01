db7eab3396368fd0717ef7ffc89da50fe4e87958
Razzberry7 <68514613+Razzberry7@users.noreply.github.com>
2022-04-01 08:53:31 -0400

    More documentation and cleaning

---
+++ CocoSplit/README.txt
@@ -0,0 +1,7 @@
+HOW TO USE COCOSPLIT
+-----------------------
+1. Go to roboflow and zip up dataset (100% training, no augments)
+2. Place zip file into presplit folder
+3. Run cocosplit
+4. Go to roboflow and create a new project; then upload everything in the splits_resized folder
+5. Now use this new project to create a model
--- CocoSplit/main.py
+++ CocoSplit/main.py
@@ -1,27 +1,31 @@
-import random
-
+### Imports ###
 import cv2
 import json
 import os
 import zipfile
+import random
 
+### Variables ###
+
+# Number of splits created from 1 original image
 num_of_splits = 10
+
+# List of the splits
 split_list = []
+
+# List of the new annotations correlating to each split
 new_annotation_list = []
 
+# List of the original images (used to keep track of what split belongs to what base image)
 image_annotation_lists = []
+
+# List of the splits' origins (needed for adjusting annotations)
 split_origins = []
 
 
+###### CODE STARTS HERE #######
 
-w_iterations = 0
-h_iterations = 0
-dimensionX = 1024
-dimensionY = 640
-
-
-# Unzip roboflow zip
-
+# Unzip roboflow zip in ./presplit folder
 path = "./presplit/"
 files = os.listdir(path)
 for file in files:
@@ -32,13 +36,12 @@ for file in files:
             zip_file.extract(names, path)
         zip_file.close()
 
-#os.remove("./presplit/*.zip")
-
-# Original annotation changing
+# Open the original JSON file, store it as a variable, and close the file
 file = open('./presplit/train/_annotations.coco.json')
 old_coco_data = json.load(file)
 file.close()
 
+# Create the new JSON file with a framework (info can be changed here if you want)
 new_coco_data = \
     {
         "info": {
@@ -77,26 +80,32 @@ new_coco_data = \
         "annotations": []
     }
 
+# Method to sort the annotations by what image they belong to (original JSON is unsorted)
 def sort_annotations():
 
-    # dictionary of annotations from original coco file
+    # Dictionary of annotations from original coco file
     annotations = old_coco_data['annotations']
 
-    # sort annotations by image_id and store it
+    # Sort annotations by image_id and store it
     sorted_annotations = sorted(annotations, key=lambda x: x['image_id'])
 
     # Call next step and pass in sorted_annotations list to be separated
     split_sorted_annotations(sorted_annotations)
 
+# Method to split the annotations up by which image they correspond to
 def split_sorted_annotations(sorted_annotations):
 
-    # dictionary of images from original coco file
+    # Dictionary of images from original coco file
     images = old_coco_data['images']
 
+    # Access global var
     global image_annotation_lists
+
+    # Create as many lists as there are original images and store that in a list
     for n in range(len(images)):
         image_annotation_lists.append([])
 
+    # Store the annotations corresponding to the list of image lists
     i = 0
     j = 0
     while i < len(sorted_annotations):
@@ -109,25 +118,28 @@ def split_sorted_annotations(sorted_annotations):
     # Call the next step to randomly split the images
     random_split()
 
+# Method to randomly split the original images
 def random_split():
 
-    # dictionary of images from original coco file
+    # Dictionary of images from original coco file
     images = old_coco_data['images']
 
+    # Create a new folder (if one doesn't exist already)
     os.mkdir("./splits")
+
+    # Delete all existing files in that folder (if any exist)
     directory = 'splits'
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         os.remove(f)
 
-    # Indices
-    i = 0
-
-    # split origins (x,y)
+    # Access global var - split origins (x,y)
     global split_origins
 
     # Nested while loop that splits images
+    i = 0
     while i < len(os.listdir('./presplit/train')) - 1:
+
         # Import image
         original_img = cv2.imread('./presplit/train/' + images[i]["file_name"])
 
@@ -136,65 +148,93 @@ def random_split():
 
         j = 0
         while j < num_of_splits:
-            # Split code (change padded_image for different picture)
 
-            #random_height = random.randint(500, 800)
-            #random_width = random.randint(900, 1200)
-            random_height = int(images[i]['height'] / 2)
-            random_width = int(images[i]['width'] / 2)
-            random_y = random.randint(0, (images[i]['height'] - random_height))
-            random_x = random.randint(0, (images[i]['width'] - random_width))
+            # Replicate robosplit by creating splits made of half the dimensions of original image
+            new_height = int(images[i]['height'] / 2)
+            new_width = int(images[i]['width'] / 2)
 
-            splits.append(original_img[random_y:(random_y + random_height), random_x:(random_x + random_width)])
+            # Generate random origin (within bounds)
+            random_y = random.randint(0, (images[i]['height'] - new_height))
+            random_x = random.randint(0, (images[i]['width'] - new_width))
+
+            # Crop the original image to create the split using the above values
+            new_split = original_img[random_y:(random_y + new_height), random_x:(random_x + new_width)]
+
+            # Append the split
+            splits.append(new_split)
+
+            # Append the split origins (we'll need this information later)
             split_origins.append([random_x, random_y])
 
             # Filename of each split
-            file_name = './splits/' + str(i) + '_' + str(j) + '_' + str(random_width) + 'x' + str(random_height) + '.jpg'
+            file_name = './splits/' + str(i) + '_' + str(j) + '_' + str(new_width) + 'x' + str(new_height) + '.jpg'
+
             # Saving the split
             cv2.imwrite(file_name, splits[j])
 
-            ####Writing the images to the JSON file
+            #### Writing the images to the JSON file ####
 
             # Create the id var
             id = hash(str(i) + "_" +  str(j))
 
             # Create the data_captured var
-            images = old_coco_data['images']
             date_captured = images[i]['date_captured']
 
-            write_new_image(id, 1, file_name, random_height, random_width, date_captured)
+            # Write new split image to the new JSON file (see helper method for more details)
+            write_new_image(id, 1, file_name, new_height, new_width, date_captured, split_list)
 
             j = (j + 1)
 
         i = (i + 1)
+
+    # Call next step to adjust the annotations
     adjust_random_annotations()
 
+# Method to adjust the annotations for each split
 def adjust_random_annotations():
+
+    # Access global vars
     global image_annotation_lists, split_origins
 
+    # Store original images as a var
     original_images = old_coco_data['images']
+
+    # Store copy of global var
     split_images = split_list
+
     split_annotations = new_annotation_list
 
-    # original_image index for grabbing the annotations list corresponding to the original image
+    # Original_image index for grabbing the annotations list corresponding to the original image
     original_image = 0
 
+    # These indices have more specific names to not get lost
     i_original_image = 0
     j_split_image = 0
     k_blueberry = 0
 
     # for loop for the number of original pictures
     for i_original_image in range(len(original_images)):
+
+        # Grab the list of annotations for a given original image
         annotation_list = image_annotation_lists[i_original_image]
+
         # for loop for the number of split images
         for j_split_image in range(num_of_splits):
-            curr_split = split_images[i_original_image * num_of_splits + j_split_image]
-            curr_split_origin = split_origins[i_original_image * num_of_splits + j_split_image]
+
+            # Calculate the current index of the split we're on
+            current_index = i_original_image * num_of_splits + j_split_image
+
+            # Grab that split & origin
+            curr_split = split_images[current_index]
+            curr_split_origin = split_origins[current_index]
 
             # for loop for the number of blueberries
             for k_blueberry in range(len(image_annotation_lists[i_original_image])):
+
+                # Grab the old annotation's bounding box and store it
                 old_annot = annotation_list[k_blueberry]['bbox']
 
+                # Store all the values in the old_annot bounding box
                 old_x = int(old_annot[0])
                 old_y = int(old_annot[1])
                 old_width = int(old_annot[2])
@@ -221,6 +261,7 @@ def adjust_random_annotations():
                         new_width1 = old_width
                 else:
                     continue
+
                 # New height
                 if (new_y1 < curr_split['height']):
                     if (new_y1 + old_height > curr_split['height']):
@@ -236,8 +277,8 @@ def adjust_random_annotations():
                 # New area
                 new_area1 = new_width1 * new_height1
 
-                # Write to JSON file
-                write_new_annotation(id1, image_id1, category_id, new_bbox1, new_area1, [], 0)
+                # Write to JSON file (see helper method for more detail)
+                write_new_annotation(id1, image_id1, category_id, new_bbox1, new_area1, [], 0, new_annotation_list)
 
                 k_blueberry = (k_blueberry + 1)
             j_split_image = (j_split_image + 1)
@@ -249,63 +290,62 @@ def adjust_random_annotations():
 # Downsize the split images (better performance in yolov5)
 def downsize():
 
+    # Make a folder (if one doesn't exist)
     os.mkdir("./splits_resized")
+
+    # Remove all files in the folder (if they exist)
     directory = 'splits_resized'
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         os.remove(f)
 
+    # Access global vars
     global split_list, new_annotation_list
 
-    # Scaling Down the image 1/3 times specifying a single scale factor.
-    scale_down = 1/3
+    # Specifying a single scale factor
+    scale_down_factor = 1/3
 
+    # List to store the new resized splits
     resized_split_list = []
-    for i in range(len(split_list)):
-        split_img = cv2.imread(split_list[i]['file_name'])
-        scaled_f_down = cv2.resize(split_img, None, fx=scale_down, fy=scale_down, interpolation=cv2.INTER_LINEAR)
 
-        # Filename of each split
-        file_name = './splits_resized/' + str(i) + '--' + str(int(split_list[i]['width'] * scale_down)) + 'x' + str(int(split_list[i]['height'] * scale_down)) + '.jpg'
+    # For loop to resize each split in the split_list
+    for i in range(len(split_list)):
+
+        # Grab the split and store it
+        split_img = cv2.imread(split_list[i]['file_name'])
+
+        # Scale that split down
+        scaled_down_split = cv2.resize(split_img, None, fx=scale_down_factor, fy=scale_down_factor, interpolation=cv2.INTER_LINEAR)
+
+        # Filename of each resized split
+        file_name = './splits_resized/' + str(i) + '--' + str(int(split_list[i]['width'] * scale_down_factor)) + 'x' + str(int(split_list[i]['height'] * scale_down_factor)) + '.jpg'
 
         # Saving the split
-        cv2.imwrite(file_name, scaled_f_down)
+        cv2.imwrite(file_name, scaled_down_split)
 
-        downsized_new_image = {
-            "id": split_list[i]['id'],
-            "license": split_list[i]['license'],
-            "file_name": file_name,
-            "height": str(int(split_list[i]['height'] * scale_down)),
-            "width": str(int(split_list[i]['width'] * scale_down)),
-            "date_captured": split_list[i]['date_captured']
-        }
-
-        resized_split_list.append(downsized_new_image)
+        # Write resized image
+        write_new_image(split_list[i]['id'], split_list[i]['license'], file_name, str(int(split_list[i]['height'] * scale_down_factor)), str(int(split_list[i]['width'] * scale_down_factor)), split_list[i]['date_captured'], resized_split_list)
 
 
+    # Resize all of the annotations in the new_annotation_list
     resized_new_annotation_list = []
     for i in range(len(new_annotation_list)):
 
-        downsized_bbox = [int(new_annotation_list[i]['bbox'][0] * scale_down), int(new_annotation_list[i]['bbox'][1] * scale_down), int(new_annotation_list[i]['bbox'][2] * scale_down), int(new_annotation_list[i]['bbox'][3] * scale_down)]
+        # Calculate new bbox and area
+        downsized_bbox = [int(new_annotation_list[i]['bbox'][0] * scale_down_factor), int(new_annotation_list[i]['bbox'][1] * scale_down_factor), int(new_annotation_list[i]['bbox'][2] * scale_down_factor), int(new_annotation_list[i]['bbox'][3] * scale_down_factor)]
         downsized_area = int(downsized_bbox[2] * downsized_bbox[3])
 
-        downsized_new_annotation = {
-            "id": new_annotation_list[i]['id'],
-            "image_id": new_annotation_list[i]['image_id'],
-            "category_id": new_annotation_list[i]['category_id'],
-            "bbox": downsized_bbox,
-            "area": downsized_area,
-            "segmentation": new_annotation_list[i]['segmentation'],
-            "iscrowd": new_annotation_list[i]['iscrowd']
-        }
-        resized_new_annotation_list.append(downsized_new_annotation)
+        # Write new annotation
+        write_new_annotation(new_annotation_list[i]['id'], new_annotation_list[i]['image_id'], new_annotation_list[i]['category_id'], downsized_bbox, downsized_area, new_annotation_list[i]['segmentation'], new_annotation_list[i]['iscrowd'], resized_new_annotation_list)
 
+    # Write everything to JSON
     write_to_json(resized_split_list, resized_new_annotation_list)
 
 
 
 ## COCO SPLIT HELPERS
-def write_new_image(id, license, file_name, height, width, date_captured):
+# Method to write new image in coco format
+def write_new_image(id, license, file_name, height, width, date_captured, split_list):
     new_image = {
         "id": id,
         "license": license,
@@ -315,13 +355,11 @@ def write_new_image(id, license, file_name, height, width, date_captured):
         "date_captured": date_captured
     }
 
-    global split_list
     split_list.append(new_image)
 
-    #new_coco_data['images'].append(new_image)
 
-
-def write_new_annotation(id, image_id, category_id, bbox, area, segmentation, iscrowd):
+# Method to write new annotation in coco format
+def write_new_annotation(id, image_id, category_id, bbox, area, segmentation, iscrowd, new_annotation_list):
     new_annotation = {
         "id" : id,
         "image_id" : image_id,
@@ -332,12 +370,10 @@ def write_new_annotation(id, image_id, category_id, bbox, area, segmentation, is
         "iscrowd" : iscrowd
     }
 
-    global new_annotation_list
     new_annotation_list.append(new_annotation)
 
-    #new_coco_data['annotations'].append(new_annotation)
 
-# separate method for appending to split_list, and writing
+# Separate method for appending to split_list, and writing
 def write_to_json(split_list, new_annotation_list):
     for i in range(len(split_list)):
         new_coco_data['images'].append(split_list[i])
@@ -346,7 +382,9 @@ def write_to_json(split_list, new_annotation_list):
         new_coco_data['annotations'].append(new_annotation_list[i])
 
 
+# Starts the program essentially
 sort_annotations()
 
+# Save everything to the new JSON file
 with open('./splits_resized/_new_annotations.coco.json', 'w') as file2:
     json.dump(new_coco_data, file2)
